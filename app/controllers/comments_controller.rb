1dccb166823b9052a731d43aa11f6734be0007ba
OscarIFZ <oscar.figueroa@alumnos.ucn.cl>
2024-12-13 11:17:36 -0300

    Switch to Bootstrap with Yarn and add development tooling.
    Replaces the `application.css` with a SCSS version importing Bootstrap and icons. Introduces `yarn` for dependency management, including related scripts for CSS build and watch. Adds `Procfile.dev` and a `bin/dev` script for streamlined local development.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,24 @@
 class CommentsController < ApplicationController
 
+  def index
+    @comments = Comment.all
+  end
+  def new
+    @comment = Comment.new
+  end
+  def create
+    @task = Task.find(params[:task_id])
+    @comment = @task.comments.new(comment_params)
+
+    if @comment.save
+
+      redirect_to @task, notice: "Comentario Agregado con Exito."
+    else
+      render :@task, status: :unprocessable_entity
+    end
+  end
+  private
+  def comment_params
+    params.require(:comment).permit(:creator, :task_id, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,17 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  #resources :tasks
+  resources :tasks do
+    resources :comments, only: [:create, :destroy]
+  end
   #
-  get "/tasks", to: "tasks#index", as: :tasks
-  get "/tasks/new" => "tasks#new", as: :new_task
-  get "/tasks/:id" => "tasks#show", as: :task
-  patch "/tasks/:id" => "tasks#update"
-  delete "/tasks/:id" => "tasks#destroy"
-  get "/tasks/:id/edit" => "tasks#edit", as: :edit_task
-  post "/tasks" => "tasks#create", as: :create_task
+  #get "/tasks", to: "tasks#index", as: :tasks
+  #get "/tasks/new" => "tasks#new", as: :new_task
+  #get "/tasks/:id" => "tasks#show", as: :task
+  #patch "/tasks/:id" => "tasks#update"
+  #delete "/tasks/:id" => "tasks#destroy"
+  #get "/tasks/:id/edit" => "tasks#edit", as: :edit_task
+  #post "/tasks" => "tasks#create", as: :create_task
 
   # Defines the root path route ("/")
   root "tasks#index"

65f57efa898f78c15cecf09ed793348c6f98d9c4
D1eku <dieku25ikari@gmail.com>
2024-12-13 13:49:54 -0300

    Add comment deletion functionality and link task titles
    Implemented a destroy action in the comments controller to allow users to delete comments and updated the views to include a delete button for comments. Additionally, task titles on the task index page are now clickable, redirecting to their respective task detail pages.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,7 @@ class CommentsController < ApplicationController
   def new
     @comment = Comment.new
   end
+
   def create
     @task = Task.find(params[:task_id])
     @comment = @task.comments.new(comment_params)
@@ -14,9 +15,21 @@ class CommentsController < ApplicationController
 
       redirect_to @task, notice: "Comentario Agregado con Exito."
     else
-      render :@task, status: :unprocessable_entity
+      redirect_to @task, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    @task = Task.find(params[:task_id])
+    @comment = @task.comments.find(params[:id])
+
+    if @comment.destroy
+      redirect_to @task, notice: "Comentario eliminado con éxito."
+    else
+      redirect_to @task, alert: "No se pudo eliminar el comentario."
+    end
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:creator, :task_id, :content)
